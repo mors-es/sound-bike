271dac3f2a845768a08020d469e158c77967822c
Dorian S <doriansomers@gmail.com>
2017-11-01 10:15:16 +0100

    clean code

--- bike-running-check/bike-running-check.ino
+++ bike-running-check/bike-running-check.ino
@@ -2,47 +2,48 @@
 // Controlling a servo position using a potentiometer (variable resistor)
 // by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
 
-#include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;  // variable to read the value from the analog pin
-int ts;
-int vol;
-int time = 0;
-int timo = 0;
-int i;
+int potpin = 0;       // analog pin used to connect the speedometer
+int val;              // variable to read the value from the analog pin
+int perimeter = 0.120 // perimeter of the weel of the bike in meter
+int totalTime = 0;    // total time just before printing the speed in milliseconds
+int time;             // time between two speedometer 'closed'
+int speed = 0;        // speed in m/s
 
 void setup()
 {
   Serial.begin(9600);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop()
 
 {
-  val = analogRead(potpin);// reads the value of the potentiometer (value between 0 and 1023)
+  val = analogRead(potpin);        // reads the value of the potentiometer (value between 0 and 1023)
 
-  val = map(val, 0, 1050, 0, 50);
- // if (val > 10){
-      Serial.println(val);
- // }
-     // scale it to use it with the servo (value between 0 and 180)
-//  for(i = 0 ;   i < 100 ; i++){
-  //if(val > 20) { ts = ts + 1;}
-  //delay(10);
-// }
-if(val > 110 ) {Serial.println("A"); time=0;};
+  val = map(val, 0, 1023, 0, 255); // map value to 8 bit
+  Serial.println(val);
+  
+  // check if the speedometer is 'closed' 
+  if(val > 200) 
+  {
+    time = millis() - totalTime;       // set time between the the last passage in the 'if'
+    speed = perimeter / (time / 1000); // set speed in m/s
+    Serial.println(speed);             // print the speed
+    totalTime = millis();              // set new time since the begining of the program
+  };
 
   if(val < 20)
-   {
-   time++;
+  {
+    time++;
   }
 
-if(time > 1000){Serial.println("B"); time=0;}
-if(time > 45){Serial.println("C"); timo=0;}// myservo.write(val);    // sets the servo position according to the scaled value
-  delay(15);
-  timo++;// waits for the servo to get there
+  // check is the time since the speedometer is 'closed' is not too long
+  if(time > 2000)
+  {
+    speed = 0;               // set the speed to zero
+    Serial.println(speed);   // print the speed (zero)
+    time=0;                  // set interval to zero.
+  }
+  // wait a bit to not send massive amounts of data but have to be enough quick to not miss the speedometer 'closed'
+  delay(20);
 }
